a99b8a0e5c9d184b51bed4daecd1aba42a8d985f
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-12 22:45:00 +0000

    Task 0 solved

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,3 +13,7 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f"{field}=(.*?){separator}",
                          f"{field}={redaction}{separator}", message)
     return message
+
+
+if __name__ == "__main__":
+    main()
