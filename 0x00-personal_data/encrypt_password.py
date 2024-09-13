77d69082296589fc94f0288de65641f9af38f313
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-13 03:14:35 +0000

    encrypt_password file updaeted

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     Hashes a password
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """
+    Checks if a password is valid
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
