a9c7af819d9e25164f2ee32d0269c8e21095f81d
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-18 15:05:48 +0000

    auth file updated

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,10 +19,12 @@ class Auth:
             return True
         if excluded_paths is None or excluded_paths == []:
             return True
-        if path in excluded_paths:
-            return False
+        # Normalize the path
         if path[-1] != '/':
             path += '/'
+        # check if the path is in the excluded paths
+        if path in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
