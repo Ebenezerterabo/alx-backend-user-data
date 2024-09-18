1395ea25c9d3b909449f69d297e9d08af0aef0d9
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-18 23:53:16 +0000

    created basic_auth and updated app file

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Basic Auth class module """
+from api.v1.auth.auth import Auth
+from typing import List
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """ Basic Auth class """
+    def __init__(self):
+        """ Initialize Basic Auth class """
+        pass
