ae5683a7444e3560a74cb25e8a2ed1a26244f681
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-18 16:27:16 +0000

    app and auth file updated

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,7 +29,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header """
-        return None
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> str:
         """ Current user """
