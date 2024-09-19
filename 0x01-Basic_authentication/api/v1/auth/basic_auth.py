badbe883a776521d340b3fd4583510be8f760b89
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-19 01:26:32 +0000

    basic_file updated

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Auth class module """
 from api.v1.auth.auth import Auth
-from typing import List
+from typing import List, TypeVar
 from models.user import User
 import base64
 
@@ -46,3 +46,19 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return None, None
         return decoded_base64_authorization_header.split(':', 1)
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """ User object from credentials """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_email, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
