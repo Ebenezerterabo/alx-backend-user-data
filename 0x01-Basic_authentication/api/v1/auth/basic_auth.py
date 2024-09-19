b8de7a248dd73dde79e9cfb8ffe5205ce77010ea
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-19 00:42:41 +0000

    basic_auth file updated

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 from typing import List
 from models.user import User
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decode base64 authorization header """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
