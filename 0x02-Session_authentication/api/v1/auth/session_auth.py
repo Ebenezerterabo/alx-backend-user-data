70b2b584bdff53ff81fef5bf741aa25e69d7f94a
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-20 01:50:55 +0000

    session_auth file updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ Session with Auth class module """
 from api.v1.auth.auth import Auth
 from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -11,16 +12,12 @@ class SessionAuth(Auth):
         """ Initialize Session Auth class """
         pass
 
-    # def create_session(self, user_id: str = None) -> str:
-    #     """ Create a session """
-    #     if user_id is None or not isinstance(user_id, str):
-    #         return None
-    #     try:
-    #         user = User.search({"id": user_id})
-    #     except Exception:
-    #         return None
-    #     if user:
-    #         session_id = self._generate_uuid()
-    #         user[0].session_id = session_id
-    #         user[0].save()
-    #         return session_id
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a session """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
