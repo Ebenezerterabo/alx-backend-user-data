06c88fdfbe6d6d40a45033fc0065355f858bc779
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-27 03:18:20 +0000

    auth file created

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Authentication module. """
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password.
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
