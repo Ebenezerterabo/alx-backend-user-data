65383075abec5bbc4c21a1675a9b6b2a57721d3e
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-27 04:15:36 +0000

    auth file updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,37 @@
 """ Authentication module. """
 
 import bcrypt
+from sqlalchemy.exc import NoResultFound
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
     """Hash a password.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initialize a new Auth instance.
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> None:
+        """Register a new user.
+        """
+        try:
+            # Check if the user already exists
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                raise ValueError(
+                    "User {} already exists".format(email)
+                )
+            self._db.add_user(email, _hash_password(password))
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+
+        return user
