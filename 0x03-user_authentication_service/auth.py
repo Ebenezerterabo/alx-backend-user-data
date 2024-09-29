f6dae640fe5f744040f309b2d1c2c6db7de7aed9
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-29 01:38:54 +0000

    file updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -117,13 +117,15 @@ class Auth:
     def update_password(reset_token: str, password: str) -> None:
         """Update a user's password.
         """
+        try:
         # Find the user by reset token
-        user = self._db.find_user_by(reset_token=reset_token)
+            user = self._db.find_user_by(reset_token=reset_token)
         # Check if the user exists
-        if user is None:
+        except NoResultFound:
             raise ValueError
         # Update the user's password
         self._db.update_user(user.id, hashed_password=_hash_password(password))
         # Commit the changes
         self._db._session.commit()
-        reset_token = None
+        user.reset_token = None
+        return None
