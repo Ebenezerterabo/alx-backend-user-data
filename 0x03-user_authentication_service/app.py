ed8324f8c99058f5687ac606e280a2e8e3846ac4
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-29 00:45:32 +0000

    file updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,7 +63,7 @@ def logout():
     # Destroy the session
     AUTH.destroy_session(user_id)
     # Return a response
-    return redirect("/", methods=['GET'])
+    return redirect("/", methods=['GET']), 302
 
 
 if __name__ == "__main__":
